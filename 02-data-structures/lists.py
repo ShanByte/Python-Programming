6fb384f8c99536f98dfe5884b7393f70fda911ee
Shantanu Kakad <163027166+ShanByte@users.noreply.github.com>
2026-03-02 20:14:39 +0530

    Created lists.py

---
+++ 02-data-structures/lists.py
@@ -0,0 +1,8 @@
+fruits = ["apple", "banana", "cherry"]
+fruits.append("orange")
+fruits.insert(1, "mango")
+fruits.remove("banana")
+
+print(f"Fruits: {fruits}")
+print(f"First fruit: {fruits[0]}")
+print(f"Last fruit: {fruits[-1]}")
