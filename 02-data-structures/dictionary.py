da9649125ee003cee68eaa57ef63185dd0df9216
Shantanu Kakad <163027166+ShanByte@users.noreply.github.com>
2026-03-03 20:44:26 +0530

    Created dictionary.py
    Give basic knowledge of dictionaries in python.

---
+++ 02-data-structures/dictionary.py
@@ -0,0 +1,11 @@
+student = 
+{
+    "name": "John",
+    "age": 20,
+    "grade": "A"
+}
+
+student["major"] = "Computer Science"
+print(f"Student: {student}")
+print(f"Name: {student['name']}")
+print(f"Keys: {student.keys()}")
