c83a8a66b4b485441ee8f475110dee66f6cb6e3e
Shantanu Kakad <163027166+ShanByte@users.noreply.github.com>
2026-03-08 20:56:42 +0530

    Created classes_objects.py

---
+++ 05-oop/classes_objects.py
@@ -0,0 +1,10 @@
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+    
+    def introduce(self):
+        return f"Hi, I'm {self.name} and I'm {self.age} years old"
+
+person1 = Person("Alice", 25)
+print(person1.introduce())
